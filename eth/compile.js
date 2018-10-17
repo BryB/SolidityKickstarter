11518af786ade3eb0cd70d04b86d7780af315858
BryB <bryanbrady6@gmail.com>
2018-10-16 21:43:14 -0700

    rebuilt the compile script

--- eth/compile.js
+++ eth/compile.js
@@ -1,8 +1,20 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':myInbox'];
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+const campaignPath = path.resolve(__dirname, 'contracts', 'Crowdfunding.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath);
+
+for (let contract in output)
+{
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.replace(':', '') + '.json'),
+    output[contract]
+  );
+}
