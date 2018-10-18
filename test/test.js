3b4f3fd49e4b5fdba420c6864479b20f0eef7d2b
BryB <bryanbrady6@gmail.com>
2018-10-17 21:33:16 -0700

    buiding boilerplate testing file

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const {interface, bytecode} = require('../compile')
+const compiledFactory = require('../eth/contracts/build/campaignFactory.json');
+const compiledCampaign = require('../eth/contracts/build/Campaign.json');
 
 const provider = ganache.provider();
 const web3 = new Web3(provider);
